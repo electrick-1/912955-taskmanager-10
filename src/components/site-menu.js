d5f2aed4eef6aab26040f4ac65ec9c2eae491442
electrick-1 <elbandido93@mail.ru>
2020-01-06 00:10:38 +0700

    реализовано закрытие окно редактирования формы

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -44,7 +44,7 @@ export default class SiteMenu {
 
   getElement() {
     if (!this._element) {
-      this._element = createElement(this.getTemplate);
+      this._element = createElement(this.getTemplate());
     }
 
     return this._element;
